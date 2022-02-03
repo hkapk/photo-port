51ada8b32943b8c58e2ce7f5a314f47f9b40ba4a
Hank Kapka <hankkapka@gmail.com>
2022-02-02 18:12:42 -0600

    added contact form and tests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -14,19 +15,29 @@ function App() {
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-      ></Nav>
+ <Nav
+  categories={categories}
+  setCurrentCategory={setCurrentCategory}
+  currentCategory={currentCategory}
+  contactSelected={contactSelected}
+  setContactSelected={setContactSelected}
+></Nav>
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
-      </main>
+      {!contactSelected ? (
+  <>
+    <Gallery currentCategory={currentCategory}></Gallery>
+    <About></About>
+  </>
+) : (
+    <ContactForm></ContactForm>
+  )}
+    </main>
     </div>
   );
 }
